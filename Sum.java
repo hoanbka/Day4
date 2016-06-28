414737946fac1a4c34be1a679d0de4637f041ed2
Albert <nguyenhoan.hut@gmail.com>
2016-06-28 15:36:06 +0700

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -6,15 +6,15 @@ public class Sum {
 
 	public static void main(String[] args) {
 		boolean check = true;
-		int tong = 0;
+		int sum = 0;
 		while (check) {
 			Scanner input = new Scanner(System.in);
 			System.out.println("Enter Integer number from keyboard :");
 			int i = input.nextInt();
-			tong = tong + i;
+			sum = sum + i;
 			// Press 0 to stop entering numbers from keyboard
 			if (i == 0) {
-				System.out.println("Sum of Integer numbers entered = " + tong);
+				System.out.println("Sum of Integer numbers entered = " + sum);
 				check = false;
 			}
 		}
